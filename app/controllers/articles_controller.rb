9a1c6526a5700eb2875a08901c7a0e4741a5dec9
Venkatakrishnan <venkatakrishnan.mari@freshworks.com>
2023-09-26 10:09:05 +0530

    First commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,57 @@
+require 'byebug'
+class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+  
+  def index
+    @articles = Article.all
+    # p @article.previous_changes[:title]
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(article_params)
+    # byebug
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+  #  byebug
+    if @article.update(article_params)
+      puts "Previous Changes: #{@article.previous_changes[:title]}"
+      puts "Previous Changes1: #{@article.saved_change_to_title?}"
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+ 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+ 
+    redirect_to articles_path
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+
+end
---
+++ app/models/article.rb
@@ -0,0 +1,27 @@
+require 'byebug'
+class Article < ApplicationRecord
+  after_save :log_changes, :logs_changes
+
+  has_many :comments, dependent: :destroy
+
+  validates :title, presence: true,
+  length: { minimum: 5 }
+
+
+  def log_changes
+    byebug
+    if previous_changes.any?
+      p "Deprecation warning not raise for previous_changes"
+    else
+      p "Failed"
+    end
+  end
+
+  def logs_changes
+    byebug
+    if saved_change_to_title?
+      p "Success"
+    end
+  end
+
+end
